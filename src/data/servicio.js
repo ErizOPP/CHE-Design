2b4cdb7108ce530701f3854c0c9941b9945c0666
ErizOPP <santiagovicenteramos@gmail.com>
2023-07-24 01:36:38 -0300

    Add Pages / Array Services / CSS
    Add Acerca De
    ROOT add more styles
    Services Card refresh sistem build

---
+++ src/data/servicio.js
@@ -0,0 +1,37 @@
+export const getServicios = async ({ filterBy } = {}) => [
+    {
+        title: 'Identidad Corporativa',
+        description: 'Identidad Corporativa, Logo, Manual de Marca y más para tu Negocio.',
+        cover: 'https://th.bing.com/th/id/OIP.NPbv2vgnQiILXRDJlXYCnAHaE1?pid=ImgDet&rs=1',
+    },
+    {
+        title: 'Maquetación Editorial',
+        description: 'Maquetación de Libros y Revistas todo el proceso creativo.',
+        cover: '	https://th.bing.com/th/id/R.65592cf3182542d6b0fe5e1ed519e331?rik=u3ZJ11UyYGWn%2bg&pid=ImgRaw&r=0',
+    },
+    {
+        title: 'Aprendiendo Git',
+        img: "https://d2sofvawe08yqg.cloudfront.net/aprendiendo-git/s_hero2x?1628527844",
+        description: 'Un libro muy interesante para aprender Git. Mucho más allá de los comandos. Habla de buenas prácticas, estrategias de trabajo en equipo y flujos para mejorar la entrega de software. Además, está escrito por un autor español, lo que siempre es de agradecer.',
+    },
+    {
+        title: 'Publicidad',
+        img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1347470803i/10284614.jpg",
+        description: '"The Clean Coder: Un Código de Conducta para Programadores Profesionales" es un libro excepcional que revoluciona la forma en que los programadores abordan su trabajo. Con sus ideas prácticas e inspiradoras, establece un nuevo estándar de profesionalismo en la industria del desarrollo de software. Su enfoque en la responsabilidad personal, el aprendizaje continuo y la comunicación efectiva, promueve la entrega de código limpio, la mejora constante y la colaboración en equipo. En resumen, este libro es una herramienta indispensable que empodera a los programadores para alcanzar la excelencia y contribuir al éxito de sus organizaciones.',
+    },
+    {
+        title: 'Flyers y Banners',
+        img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1328834793i/2998152.jpg',
+        description: '"JavaScript: The Good Parts" es un libro imprescindible que destaca las mejores prácticas y características esenciales de JavaScript. A través de una visión concisa y perspicaz, el autor nos guía por el mundo de este lenguaje, revelando su potencial y resaltando sus aspectos más poderosos y efectivos. Con un enfoque en la simplicidad y la elegancia, el libro ofrece una guía práctica para aprovechar al máximo las partes más sólidas y confiables de JavaScript, permitiéndonos escribir un código claro y eficiente. En resumen, esta obra es una valiosa herramienta para cualquier desarrollador que desee dominar el arte de programar con JavaScript.',
+    },
+    {
+        title: 'Infografía',
+        description: '',
+        img: '',
+    },
+    {
+        title: 'Ilustración',
+        description: '',
+        img: '',
+    },
+]
