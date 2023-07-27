1511df7bfd1a846f8d384fe78ad98b8493168c8c
ErizOPP <santiagovicenteramos@gmail.com>
2023-07-27 00:51:32 -0300

    Ico-Font | Optimize index | Services img css | Service folder and .md subpages
    Ico-Font: Add font for social ico in about me page
    
    Optimize index, solve problem to responsive and tiny display
    
    Services img css: add full cover img traslate up
    Effect easy in out
    
    Service folder and .md subpages: Create folder service, and index.astro, and *.md for before subpages.

---
+++ public/para agregar.md
@@ -0,0 +1,7 @@
+# Servicio > Cada Servicio
+Dentro de cada Servicio colocar información explicativa de cara a que el futuro cliente tenga un paso previo al contratar, para aprender por si algo no lo sabia.
+Ejemplo de información: https://es.venngage.com/blog/que-es-una-infografia/
+
+
+# Para ver para Servicios, textos, info, ejemplos
+https://dulqui-design-studio.mailerpage.io/
---
+++ src/data/portfolio-notion.js
@@ -0,0 +1,31 @@
+import { Client } from '@notionhq/client'
+
+const DATABASE_ID = 'dbc937e6b46844009dc053d8efd02dcf'
+
+const notion = new Client({
+    auth: import.meta.env.NOTION_TOKEN
+})
+
+export const getPortfolio = async ({ filterBy } = {}) => {
+    const query = { database_id: DATABASE_ID}
+
+    if (filterBy) {
+        query.filter = {
+        property: 'slug',
+        rich_text: {
+            equals: filterBy
+            }
+        }
+    }
+    const { results } = await notion.databases.query(query)
+
+    return results.map(page => {
+        const { properties } = page
+        const { slug, title } = properties;
+        console.log(results)
+        return {
+            id: slug.rich_text[0].plain_text,
+            title: title.title[0].plain_text,
+        }
+    })
+}
