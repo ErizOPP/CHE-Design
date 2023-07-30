858a26f4afc54aae33aa3677da1001b4fa09db9d
ErizOPP <82858055+ErizOPP@users.noreply.github.com>
2023-07-30 18:42:42 -0300

    Add Page Tarifa
    Add Page Tarifa without link-nav

--- src/data/servicio.js
+++ src/data/servicio.js
@@ -3,42 +3,48 @@ export const servicio = [
         title: 'Marca Corporativa',
         description: 'Identidad Corporativa, Logo, Manual de Marca y más para tu Negocio.',
         img: '/servicios/cover-branding.webp',
-        href: 'servicios/branding'
+        href: '/servicios/branding'
     },
     {
         title: 'Maquetación Editorial',
         description: 'Maquetación de Libros, Brochuré y Revistas, Diseño de Tapa y Contratapa.',
         img: "/servicios/cover-editorial.webp",
-        href: 'servicios/editorial'
-    },
-    {
-        title: 'Infografía',
-        description: 'Representación visual de información y datos, en gráficos, barras, mapas, etc.',
-        img: "/servicios/cover-infog.webp",
-        href: 'servicios/infografias'
+        href: '/servicios/editorial'
     },
     {
         title: 'Desarrollo Web',
         img: "/servicios/cover-web.webp",
         description: 'Creación de paginas estáticas para landingpages, uso de Astro Build para blogs y Wordpress para tiendas web. ',
-        href: 'servicios/publicidad'
+        href: '/servicios/publicidad'
+    },
+    {
+        title: 'Infografía',
+        description: 'Representación visual de información y datos, en gráficos, barras, mapas, etc.',
+        img: "/servicios/cover-infog.webp",
+        href: '/servicios/infografias'
+    },
+    {
+        title: 'Packaging',
+        description: 'Diseño de producto, bolsas, cajas, etiquetas, y más.',
+        img: "/servicios/cover-packaging.webp",
+        href: '/servicios/packaging'
     },
     {
         title: 'Publicidad',
-        img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1347470803i/10284614.jpg",
-        description: '"The Clean Coder: Un Código de Conducta para Programadores Profesionales" es un libro excepcional que ',
-        href: 'servicios/publicidad'
+        description: 'Diseño de producto, bolsas, cajas, etiquetas, y más.',
+        img: "/servicios/cover-editorial.webp",
+        href: '/servicios/publicidad'
     },
     {
         title: 'Flyers y Banners',
-        img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1328834793i/2998152.jpg',
-        description: '"JavaScript: The Good Parts" es un libro imprescindible que destaca las mejores prácticas y caracter',
-        href: 'servicios/flyers-banners'
+        img: '/servicios/cover-flyers-banners.webp',
+        description: 'Para digital o impresos, diseño de flyers con la información que nos proporciones.',
+        href: '/servicios/flyers-banners'
     },
     {
         title: 'Ilustración',
         description: 'Uso de Procreate para generar Ilustraciones Realistas.',
-        img: '',
-        href: 'servicios/ilustracion'
+        img: '/servicios/cover-ilustracion.webp',
+        href: '/servicios/ilustracion'
     },
 ]
--- src/pages/servicios/editorial.md
+++ src/pages/servicios/editorial.md
@@ -1,4 +1,5 @@
 ---
+layout: '../../layouLServ.astro'
 title: 'Maquetación Editorial'
 description: 'Este es el primer post de mi nuevo blog Astro.'
 image:
@@ -6,6 +7,12 @@ image:
     alt: 'El logotipo de Astro con la palabra One.'
 tags: ["astro", "bloguear", "aprender en público"]
 ---
+
+# Marca Corporativa
+#### Diseño de la estética general de la marca y Manual de Marca: diseño de logotipo Elección de tipografías, paleta cromática y diseño de elementos gráficos decorativos. Recomendaciones de Buenas Practicas de uso con tu marca. Mock Up para que veas brillar tu marca en la vida real
+
+
+
 # Mi primer post en el blog
 
 
@@ -24,3 +31,50 @@ tags: ["astro", "bloguear", "aprender en público"]
 ## Próximos pasos
 
 Terminaré el tutorial de Astro, y luego seguiré añadiendo más posts. Mira este espacio para más por venir.
+
+
+# This is a level 1 heading
+## This is a level 2 heading
+### This is a level 3 heading
+
+This is a paragraph.
+
+This is another paragraph.
+
+- This is a bullet point.
+- This is another bullet point.
+
+1. This is an ordered list.
+2. This is another ordered list.
+
+| Column 1 | Column 2 |
+| --- | --- |
+| Row 1 Column 1 | Row 1 Column 2 |
+| Row 2 Column 1 | Row 2 Column 2 |
+
+I just love **bold text**.
+
+A*cat*meow
+
+This text is ***really important***.
+
+> Dorothy followed her through many of the beautiful rooms in her castle.
+
+> #### The quarterly results look great!
+>
+> - Revenue was off the chart.
+> - Profits were higher than ever.
+>
+>  *Everything* is going according to **plan**.
+
+1. First item
+2. Second item
+3. Third item
+    1. Indented item
+    2. Indented item
+4. Fourth item
+
+ ![Tux, the Linux mascot](/assets/images/tux.png)
+
+ ---
+
